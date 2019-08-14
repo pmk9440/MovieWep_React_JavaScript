ffb777741d8729b69d7c3e21fa10f6c02adc5967
박민규 <pmk9440@naver.com>
2019-08-14 17:19:08 +0900

    8/14 study

--- src/App.js
+++ src/App.js
@@ -23,7 +23,17 @@ const movies = [
 ]
 
 class App extends Component {
+
+  componentWillMount() {
+    console.log("will mount")
+  }
+
+  componentDidMount() {
+    console.log("Did mount")
+  }
+
   render() {
+    console.log("will render")
     return (
       <div className="App">
         {movies.map((movie, index) => {
